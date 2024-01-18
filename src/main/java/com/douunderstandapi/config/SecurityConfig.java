b8c0598c2634f02ce32ff50d1e85bc9b92dc452d
soonhankwon <soonable@naver.com>
2024-01-18 12:36:58 +0900

    feat: SecurityConfig - Security cors 설정 적용(custom_ - JwtVerification filter 적용 - auth path whitelist 추가

--- src/main/java/com/douunderstandapi/config/SecurityConfig.java
+++ src/main/java/com/douunderstandapi/config/SecurityConfig.java
@@ -1,30 +1,39 @@
 package com.douunderstandapi.config;
 
-import com.douunderstandapi.common.security.dsl.JwtFilterConfig;
+import com.douunderstandapi.auth.service.AuthService;
+import com.douunderstandapi.common.security.filter.JwtVerificationFilter;
 import com.douunderstandapi.common.security.handler.CustomAuthenticationEntryPoint;
 import com.douunderstandapi.common.security.handler.CustomLogoutSuccessHandler;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtFilterConfig jwtFilterConfig;
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPointHandler;
     private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+    private final AuthService authService;
 
     private static final String[] AUTH_WHITELIST = {
+            "/api/v1/auth/**",
             "/api/v1/users/auth-email/**",
             "/api/v1/users/**",
             "/h2-console/**",
@@ -32,15 +41,10 @@ public class SecurityConfig {
     };
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.apply(jwtFilterConfig);
 
         return http
+                .cors(customCorsConfig())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
@@ -58,6 +62,32 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(
                         exception -> exception.authenticationEntryPoint(customAuthenticationEntryPointHandler))
+                .addFilterBefore(new JwtVerificationFilter(authService),
+                        UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public Customizer<CorsConfigurer<HttpSecurity>> customCorsConfig() {
+        return httpSecurityCorsConfigurer -> {
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
+        };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET", "DELETE", "PATCH", "PUT", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addExposedHeader("*");
+        corsConfiguration.addExposedHeader(HttpHeaders.AUTHORIZATION);
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
