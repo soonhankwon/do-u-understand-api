61ea4da4f7d0dd56aab625207de9ebd672ff387a
soonhankwon <soonable@naver.com>
2024-01-18 12:55:28 +0900

    feat: 에러코드 추가(#21)

--- src/main/java/com/douunderstandapi/common/enumtype/ErrorCode.java
+++ src/main/java/com/douunderstandapi/common/enumtype/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 
     //400
     NOT_EXIST_USER_EMAIL(4000, "유저 이메일이 존재하지 않습니다."),
-    NOT_EXIST_KNOWLEDGE_ID(4001, "지식 아이디가 존재하지 않습니다."),
+    NOT_EXIST_POST_ID(4001, "게시글 아이디가 존재하지 않습니다."),
     ENCRYPTION_FAILED(4002, "암호화에 실패하였습니다."),
     DECRYPTION_FAILED(4003, "복호화에 실패하였습니다."),
     CRYPTOGRAPHY_FAILED(4004, "암복호화에 실패하였습니다"),
@@ -19,6 +19,8 @@ public enum ErrorCode {
     INVALID_TOKEN(4008, "유효한 토큰이 아닙니다."),
     LOGIN_REQUIRED_FIRST(4009, "로그인이 필요합니다."),
     NO_AUTH_ACCESS(4010, "접근할 수 있는 권한이 없습니다."),
+    NOT_EXIST_COOKIE(4010, "쿠키가 없습니다. 로그인을 해주세요."),
+    COOKIE_EXPIRATION(4011, "만료된 쿠키입니다."),
 
     //500
     UNKNOWN(5000, "서버 내부 에러가 발생했습니다."),
