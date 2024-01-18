612e0c6f2c6dac2e1e9e34e11fe8c10004681d05
soonhankwon <soonable@naver.com>
2024-01-18 12:37:57 +0900

    feat: AuthController (#22) - 로그인, 로그아웃, 이메일 인증 엔드포인트

---
+++ src/main/java/com/douunderstandapi/auth/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.douunderstandapi.auth.controller;
+
+import com.douunderstandapi.auth.dto.request.AuthEmailRequest;
+import com.douunderstandapi.auth.dto.request.AuthLoginRequest;
+import com.douunderstandapi.auth.dto.response.AuthEmailResponse;
+import com.douunderstandapi.auth.dto.response.AuthLoginResponse;
+import com.douunderstandapi.auth.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthLoginResponse> login(@RequestBody AuthLoginRequest request,
+                                                   HttpServletResponse httpServletResponse) {
+
+        AuthLoginResponse res = authService.login(request, httpServletResponse);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @PostMapping("/email")
+    public ResponseEntity<AuthEmailResponse> authEmail(@RequestBody AuthEmailRequest request) {
+
+        AuthEmailResponse res = authService.authEmail(request);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthLoginResponse> refresh(HttpServletRequest httpServletRequest,
+                                                     HttpServletResponse httpServletResponse) {
+
+        AuthLoginResponse res = authService.refresh(httpServletRequest, httpServletResponse);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse) {
+        //로그아웃 로직
+        return ResponseEntity.ok().body("logout");
+    }
+}
