7d7fa20cc10d1991e177b62fbbfc988de3dd9b33
soonhankwon <soonable@naver.com>
2024-01-22 13:16:03 +0900

    feat: Category 레포지토리

---
+++ src/main/java/com/douunderstandapi/category/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.douunderstandapi.category.repository;
+
+import com.douunderstandapi.category.domain.Category;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
