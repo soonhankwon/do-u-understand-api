e1b482b4aef85459d623224f493f7a2ba0732cd1
soonhankwon <soonable@naver.com>
2024-01-22 19:09:27 +0900

    config: whitelist path 변경 - addUser 외 토큰 인증필요

--- src/main/java/com/douunderstandapi/config/SecurityConfig.java
+++ src/main/java/com/douunderstandapi/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/api/v1/auth/**",
-            "/api/v1/users/**",
+            "/api/v1/users",
             "/h2-console/**",
             "/index.html"
     };
