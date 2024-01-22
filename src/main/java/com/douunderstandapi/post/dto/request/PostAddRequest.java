36de92c269016463e23b3df176bed87ae166cdb6
soonhankwon <soonable@naver.com>
2024-01-22 13:19:23 +0900

    feat: Post Add api spec 추가 - Category List: categoriesName

--- src/main/java/com/douunderstandapi/post/dto/request/PostAddRequest.java
+++ src/main/java/com/douunderstandapi/post/dto/request/PostAddRequest.java
@@ -1,18 +1,22 @@
 package com.douunderstandapi.post.dto.request;
 
+import com.douunderstandapi.category.domain.Category;
 import com.douunderstandapi.post.domain.Post;
 import com.douunderstandapi.user.domain.User;
+import java.util.List;
 
 public record PostAddRequest(
         String title,
         String content,
-        String link
+        String link,
+        List<String> categoriesName
 ) {
-    public Post toEntity(User user) {
+    public Post toEntity(User user, Category category) {
         return Post.of(
                 this.title,
                 this.content,
                 this.link,
-                user);
+                user,
+                category);
     }
 }
