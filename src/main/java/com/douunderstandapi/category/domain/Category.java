c1e656beb0f3538111126024b5978e3b629ab862
soonhankwon <soonable@naver.com>
2024-01-22 13:15:43 +0900

    feat: Category 도메인

---
+++ src/main/java/com/douunderstandapi/category/domain/Category.java
@@ -0,0 +1,27 @@
+package com.douunderstandapi.category.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "`category`")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
