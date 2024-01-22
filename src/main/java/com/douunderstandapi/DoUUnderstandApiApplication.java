2aa6c95d5fcf4bb6a536c08bab35d7f61c8d12f5
soonhankwon <soonable@naver.com>
2024-01-22 15:28:23 +0900

    feat: Mock 데이터 생성 코드 삭제

--- src/main/java/com/douunderstandapi/DoUUnderstandApiApplication.java
+++ src/main/java/com/douunderstandapi/DoUUnderstandApiApplication.java
@@ -1,45 +1,13 @@
 package com.douunderstandapi;
 
-import com.douunderstandapi.post.dto.request.PostAddRequest;
-import com.douunderstandapi.post.repository.PostRepository;
-import com.douunderstandapi.user.domain.User;
-import com.douunderstandapi.user.dto.request.UserAddRequest;
-import com.douunderstandapi.user.repository.UserRepository;
-import com.douunderstandapi.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class DoUUnderstandApiApplication implements CommandLineRunner {
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    PostRepository postRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        UserAddRequest userAddRequest = new UserAddRequest("soonable@gmail.com", "1", "1", true);
-        userService.addUser(userAddRequest);
-
-        for (int i = 1; i < 50; i++) {
-            PostAddRequest request = new PostAddRequest("title" + i,
-                    "content" + i,
-                    "link" + i);
-            User user = userRepository.findByEmail("soonable@gmail.com")
-                    .orElseThrow();
-            postRepository.save(request.toEntity(user));
-        }
-    }
-
+public class DoUUnderstandApiApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(DoUUnderstandApiApplication.class, args);
     }
