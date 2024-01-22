30d617d6952348a058121d89ee9ce3e3e51265d9
soonhankwon <soonable@naver.com>
2024-01-22 15:29:35 +0900

    config: 불필요 URI 삭제

--- src/main/java/com/douunderstandapi/config/SecurityConfig.java
+++ src/main/java/com/douunderstandapi/config/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/api/v1/auth/**",
-            "/api/v1/users/auth-email/**",
             "/api/v1/users/**",
             "/h2-console/**",
             "/index.html"
