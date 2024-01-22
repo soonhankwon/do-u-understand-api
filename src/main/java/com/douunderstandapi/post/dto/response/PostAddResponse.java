b2ac17ad198ac4d999236c9b52a2f390d3808fc4
soonhankwon <soonable@naver.com>
2024-01-22 13:20:17 +0900

    feat: Post Add response api spec 추가 - userEmail - commentCount - categoryName

--- src/main/java/com/douunderstandapi/post/dto/response/PostAddResponse.java
+++ src/main/java/com/douunderstandapi/post/dto/response/PostAddResponse.java
@@ -4,20 +4,26 @@ import com.douunderstandapi.post.domain.Post;
 
 public record PostAddResponse(
         Long id,
+        String userEmail,
         String title,
         String content,
-        String link
+        String link,
+        int commentCount,
+        String categoryName
 ) {
-    public static PostAddResponse of(Long id, String title, String content, String link) {
-        return new PostAddResponse(id, title, content, link);
+    public static PostAddResponse of(Long id, String userEmail, String title, String content, String link,
+                                     String category) {
+        return new PostAddResponse(id, userEmail, title, content, link, 0, category);
     }
 
     public static PostAddResponse from(Post post) {
         return PostAddResponse.of(
                 post.getId(),
+                post.getUser().getEmail(),
                 post.getTitle(),
                 post.getContent(),
-                post.getLink()
+                post.getLink(),
+                post.getCategory().getName()
         );
     }
 }
