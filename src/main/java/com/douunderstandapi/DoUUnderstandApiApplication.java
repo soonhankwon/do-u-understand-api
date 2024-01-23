f0c983d5e6d9a0c3c2b608971f150f7b537ba209
soonhankwon <soonable@naver.com>
2024-01-23 23:48:26 +0900

    feat: TimeZone - Asia/Seoul

--- src/main/java/com/douunderstandapi/DoUUnderstandApiApplication.java
+++ src/main/java/com/douunderstandapi/DoUUnderstandApiApplication.java
@@ -1,5 +1,7 @@
 package com.douunderstandapi;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,7 +9,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class DoUUnderstandApiApplication {
-    
+
+    @PostConstruct
+    void start() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DoUUnderstandApiApplication.class, args);
     }
