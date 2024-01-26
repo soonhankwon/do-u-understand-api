559afb448bb372ad975fd7f8b073110fa3775342
soonhankwon <soonable@naver.com>
2024-01-26 12:01:59 +0900

    test: 포스트 목록 조회 테스트 - WebMvcTest - API 문서 업데이트(RestDocs)

--- src/test/java/com/douunderstandapi/post/controller/PostControllerTest.java
+++ src/test/java/com/douunderstandapi/post/controller/PostControllerTest.java
@@ -1,12 +1,14 @@
 package com.douunderstandapi.post.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
@@ -18,14 +20,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.douunderstandapi.annotation.WithUserPrincipals;
+import com.douunderstandapi.post.dto.PostDTO;
 import com.douunderstandapi.post.dto.request.PostAddRequest;
 import com.douunderstandapi.post.dto.request.PostUpdateRequest;
 import com.douunderstandapi.post.dto.response.PostAddResponse;
 import com.douunderstandapi.post.dto.response.PostGetResponse;
 import com.douunderstandapi.post.dto.response.PostUpdateResponse;
+import com.douunderstandapi.post.dto.response.PostsGetResponse;
 import com.douunderstandapi.post.service.PostService;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -111,6 +118,92 @@ class PostControllerTest {
                         )
                 )
                 .andExpect(status().isCreated());
+    }
+
+    @DisplayName("{GET} 포스트 목록 조회 - 정상호출")
+    @WithUserPrincipals
+    @Test
+    void getPosts() throws Exception {
+        String categoryName1 = "java";
+        String categoryName2 = "jpa";
+        String categoryName3 = "web";
+        when(postService.findPosts(anyString(), anyInt(), anyString()
+                , anyString()))
+                .thenReturn(createPostsGetResponse(List.of(categoryName1, categoryName2, categoryName3)));
+
+        mvc.perform(
+                        RestDocumentationRequestBuilders.get("/api/v1/posts")
+                                .param("pageNumber", "1")
+                                .param("mode", "my")
+                                .param("query", "jpa")
+                                .with(csrf().asHeader())
+                                .header(HttpHeaders.AUTHORIZATION, UUID.randomUUID().toString()))
+                .andDo(print())
+                .andDo(
+                        document(
+                                "get-posts",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                responseFields(
+                                        fieldWithPath("totalPageCount")
+                                                .type(NUMBER)
+                                                .description("페이지 카운트"),
+                                        fieldWithPath("postList")
+                                                .type(ARRAY)
+                                                .description("포스트 정보"),
+                                        fieldWithPath("postList[].id")
+                                                .type(NUMBER)
+                                                .description("포스트 ID"),
+                                        fieldWithPath("postList[].title")
+                                                .type(STRING)
+                                                .description("제목"),
+                                        fieldWithPath("postList[].content")
+                                                .type(STRING)
+                                                .description("컨텐츠"),
+                                        fieldWithPath("postList[].link")
+                                                .type(STRING)
+                                                .description("관련링크"),
+                                        fieldWithPath("postList[].commentCount")
+                                                .type(NUMBER)
+                                                .description("코멘트 카운트"),
+                                        fieldWithPath("postList[].userEmail")
+                                                .type(STRING)
+                                                .description("이메일"),
+                                        fieldWithPath("postList[].userId")
+                                                .type(NUMBER)
+                                                .description("유저 ID"),
+                                        fieldWithPath("postList[].createdAt")
+                                                .type(STRING)
+                                                .description("작성일시"),
+                                        fieldWithPath("postList[].categoryName")
+                                                .type(STRING)
+                                                .description("카테고리 이름"),
+                                        fieldWithPath("postList[].subscribeMe")
+                                                .type(BOOLEAN)
+                                                .description("구독여부")
+                                )
+                        )
+                )
+                .andExpect(status().isOk());
+    }
+
+    private PostsGetResponse createPostsGetResponse(List<String> categoryNames) {
+        String createdAt = LocalDateTime.now().toString();
+        List<PostDTO> posts = LongStream.range(1, 3)
+                .mapToObj(i -> PostDTO.builder()
+                        .id(i)
+                        .title("RESTful API" + i)
+                        .content("Restful API란....." + i)
+                        .link("https://selfnews/2in2" + i)
+                        .commentCount(i)
+                        .userEmail("selfnews" + i + "@gmail.com")
+                        .userId(i)
+                        .createdAt(createdAt)
+                        .subscribeMe(true)
+                        .categoryName(categoryNames.get((int) i))
+                        .build())
+                .collect(Collectors.toList());
+        return new PostsGetResponse(1, posts);
     }
 
     @DisplayName("{GET} 포스트 조회(상세) - 정상호출")
